91febcda5666651f0c29e74e4a5f5c8d3ecbac2a
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-05-05 23:27:39 +0300

    made Form component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+// import css from '.ContactForm/ContactForm.module.css';
 import ContactForm from './ContactForm/ContactForm';
 class App extends Component {
+  state = {
+    contacts: [],
+    // name: '',
+    // number: '',
+  };
+  formSubmitHandler = data => {
+    console.log(data);
+  };
+
   render() {
     return (
       <div style={{ paddingLeft: 15 }}>
         <h1 className="Contact_title">Phonebook</h1>
-        <ContactForm />
+        <ContactForm onSubmit={this.formSubmitHandler} />
         <h2 className="Contact__title">Contacts</h2>
         <ul className="Contact__list"></ul>
       </div>
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,22 +1,61 @@
+import { Component } from 'react';
 import css from './ContactForm.module.css';
 
-export default function ContactForm() {
-  return (
-    <form className={css.contact__form}>
-      <label className={css.contact__lable}>
-        Name
-        <input
-          className={css.contact__input}
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-      </label>
-      <button className={css.contact__btn} type="submit">
-        Add contact
-      </button>
-    </form>
-  );
+class ContactForm extends Component {
+  state = { name: '', number: '' };
+
+  hendlChange = e => {
+    console.log(e.currentTarget.name);
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  hendlSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    this.resetForm();
+  };
+
+  resetForm() {
+    this.setState({ name: '', number: '' });
+  }
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <form className={css.contact__form} onSubmit={this.hendlSubmit}>
+        <label className={css.contact__lable}>
+          Name
+          <input
+            className={css.contact__input}
+            value={name}
+            onChange={this.hendlChange}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <label className={css.contact__lable}>
+          Number
+          <input
+            className={css.contact__input}
+            value={number}
+            onChange={this.hendlChange}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button className={css.contact__btn} type="submit">
+          Add contact
+        </button>
+      </form>
+    );
+  }
 }
+
+export default ContactForm;
