a0cf20d74b95804073cb696cafe7d1004097218a
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-05-06 14:32:44 +0300

    add Filter with styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
 import ContactList from './ContactList/ContactList';
 import ContactForm from './ContactForm/ContactForm';
+import Filer from './Filter/Filter';
 class App extends Component {
   state = {
     contacts: [],
+    filter: '',
   };
   formSubmitHandler = data => {
     const preCheck = this.state.contacts.some(
@@ -31,13 +33,10 @@ class App extends Component {
         <h1 className="contact_title">Phonebook</h1>
         <ContactForm onSubmit={this.formSubmitHandler} />
         <h2 className="contact__title">Contacts</h2>
+        <Filer value={this.filter} />
         <ul className="contact__list">
-          {contacts.map(contact => (
-            <ContactList
-              key={contact.id}
-              name={contact.name}
-              number={contact.number}
-            />
+          {contacts.map(({ id, name, number }) => (
+            <ContactList key={id} name={name} number={number} />
           ))}
         </ul>
       </div>
